df57385aba30e096398ef03f60dfc298ef579d71
Dhinakar Panneer Selvam <dhinakarintech@gmail.com>
2016-01-14 11:08:50 +0530

    generate file changes

--- ios_build/generate.sh
+++ ios_build/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/admin/gradle/bin
 rm -rf ~/filePath
-ln -s /Users/admin/Desktop/Sloan_test_Dhina_iOS ~/filePath 
+ln -s /Users/admin/Desktop/Sloan_App_test_iOS  ~/filePath 
 cd ~/filePath
 echo 'Changed the file directory to project directory'
 gradle -q shareTheApp
